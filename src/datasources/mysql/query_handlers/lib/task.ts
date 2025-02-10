06d01b63d632a65ffa64843e1ce869788eb27357
saikumardev061996 <saikumar061996@gmail.com>
2025-02-10 08:58:49 +0530

    bug fixes

--- src/datasources/mysql/query_handlers/lib/task.ts
+++ src/datasources/mysql/query_handlers/lib/task.ts
@@ -7,7 +7,7 @@ const TAG = 'data_store_mysql_lib_task';
 export async function checkTaskTitleExist(connection : PoolClient,title : string): Promise<any> {
     logger.info(`${TAG}.checkTaskTitleExist()`);
     try{
-        const query : string = `select * from task where title ILIKE '%${title}%' and completed_at IS NULL`;
+        const query : string = `select * from tasks where title ILIKE '%${title}%' and completed_at IS NULL`;
         const result = await fetchRecord(connection, query, [])
         return result
     }catch(error){
@@ -19,7 +19,7 @@ export async function checkTaskTitleExist(connection : PoolClient,title : string
 export async function checkTaskIdExist(connection : PoolClient, taskId : number) : Promise<any>{
     logger.info(`${TAG}.checkTaskIdExist()`);
     try{
-        const query : string = `select * from task where task_id = $1`;
+        const query : string = `select * from tasks where task_id = $1`;
         const result = await fetchRecord(connection, query, [taskId]);
         return result;
     }catch(error){
@@ -31,7 +31,7 @@ export async function checkTaskIdExist(connection : PoolClient, taskId : number)
 export async function saveTask(connection : PoolClient, task) : Promise<any>{
     logger.info(`${TAG}.saveTask()`);
     try{
-        const query : string = `INSERT INTO task (title, description, due_date) VALUES ($1, $2, $3)`
+        const query : string = `INSERT INTO tasks (title, description, due_date) VALUES ($1, $2, $3)`
         const result = await saveRecord(connection, query, [
             task.title,
             task.description,
@@ -47,7 +47,7 @@ export async function saveTask(connection : PoolClient, task) : Promise<any>{
 export async function getTasks(connection : PoolClient, search? : string): Promise<any> {
     logger.info(`${TAG}.getTasks()`);
     try{
-        let query : string = `select * from task`
+        let query : string = `select * from tasks`
         if(search){
             query = query + ` WHERE title ILIKE '%${search}%' OR description ILIKE '%${search}%'`
         }
@@ -63,7 +63,7 @@ export async function getTasks(connection : PoolClient, search? : string): Promi
 export async function updateTask(connection : PoolClient, task : any, taskId : number) : Promise<any>{
     logger.info(`${TAG}.updateTask()`);
     try{
-        const query : string = `UPDATE task SET title = $1, description = $2, due_date = $3, updated_at = NOW() WHERE task_id = $4`
+        const query : string = `UPDATE tasks SET title = $1, description = $2, due_date = $3, updated_at = NOW() WHERE task_id = $4`
         const result = await updateRecord(connection, query, [
             task.title,
             task.description,
@@ -80,7 +80,7 @@ export async function updateTask(connection : PoolClient, task : any, taskId : n
 export async function updateTaskStatus(connection : PoolClient, taskId : number, taskStatus : string) : Promise<any>{
     logger.info(`${TAG}.updateTaskStatus()`);
     try{
-        const query : string = `UPDATE task SET status = $1, completed_at = NOW() WHERE task_id = $2`
+        const query : string = `UPDATE tasks SET status = $1, completed_at = NOW() WHERE task_id = $2`
         const result = await updateRecord(connection, query, [
             taskStatus,
             taskId 
@@ -96,7 +96,7 @@ export async function updateTaskStatus(connection : PoolClient, taskId : number,
 export async function deleteTask(connection : PoolClient, taskId : number) : Promise<any>{
     logger.info(`${TAG}.deleteTask()`);
     try{
-        const query : string = `DELETE FROM  task  WHERE task_id = $1`
+        const query : string = `DELETE FROM  tasks  WHERE task_id = $1`
         const result = await deleteRecord(connection, query, [
             taskId 
         ])
